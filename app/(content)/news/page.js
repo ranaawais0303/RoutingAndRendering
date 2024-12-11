dff2c449c93aa8a2ee8a39859881d084f4d44039
rana awais <r.awais@pionlog.com>
2024-12-11 18:14:41 +0500

    fetch data directly next server component instead make it client component

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,47 +1,18 @@
-"use client";
 import NewsLists from "@/components/news/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
-import { useEffect, useState } from "react";
 
-const NewsPage = () => {
-  const [news, setNews] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-  const fetchNews = async () => {
-    setLoading(true);
-    const response = await fetch("http://localhost:8080/news");
-    if (!response.ok) {
-      setError("Failed to fetch news");
-      setLoading(false);
-    }
+const NewsPage = async () => {
+  const response = await fetch("http://localhost:8080/news");
 
-    setLoading(false);
-    const data = await response.json();
-    setNews(data);
-  };
-
-  useEffect(() => {
-    fetchNews();
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
+  if (!response.ok) {
+    throw new Error(`Failed to fetch news.`);
   }
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsLists newsItems={news} />;
-  }
+  const news = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsLists newsItems={news} />
     </>
   );
 };
