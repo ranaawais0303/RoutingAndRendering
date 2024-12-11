a0808f734c476cc7dfc8eafd1cf3c082c66cf6cd
rana awais <r.awais@pionlog.com>
2024-12-11 12:10:50 +0500

    for image settings for full screen

--- app/(content)/news/[slug]/image/page.js
+++ app/(content)/news/[slug]/image/page.js
@@ -5,14 +5,13 @@ const ImagePage = ({ params }) => {
   const newsSlug = params?.slug;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
 
- 
-
   if (!newsItem) {
+    console.log(newsItem, "page not found here");
     notFound();
   }
   return (
     <div className="fullscreen-image">
-      <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+      <img src={`/images/news/${newsItem?.image}`} alt={newsItem?.title} />
     </div>
   );
 };
